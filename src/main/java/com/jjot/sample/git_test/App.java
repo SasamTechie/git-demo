e64454aaf04aac16e0ee16014460d33686254712
SasamTechie <sasam.jd214@gmail.com>
2024-07-09 23:04:10 +0530

    added git-test repo to local repositary succesfull

---
+++ src/main/java/com/jjot/sample/git_test/App.java
@@ -0,0 +1,19 @@
+package com.jjot.sample.git_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+
+{
+
+	public void addData(String data) {
+
+		System.out.println("data=" + data);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
