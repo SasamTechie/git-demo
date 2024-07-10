dd5b816eb664d75c6290737cf6b50e2bee89b87d
SasamTechie <sasam.jd214@gmail.com>
2024-07-10 11:55:12 +0530

    if condition added succesfully to local repository

--- src/main/java/com/jjot/sample/git_test/App.java
+++ src/main/java/com/jjot/sample/git_test/App.java
@@ -11,6 +11,12 @@ public class App
 	public void addData(String data) {
 
 		System.out.println("data=" + data);
+
+		if (data.equals("anil")) {
+
+			System.out.println("Welcome");
+		}
+
 	}
 
 	public static void main(String[] args) {
